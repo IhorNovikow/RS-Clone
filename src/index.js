056ec59b59104268784b739b5859488167a5d9dd
vikuli <vikukulichka.terekhova@gmail.com>
2022-08-19 22:04:07 +0300

    feat: edit form for registration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+import './style/normalize.css';
 import './style/style.css';
 
-let t = document.querySelector('p');
-t.style.transform = 'rotateY(0) scale(1.1);';
-console.log(t)
+/*const inp = document.querySelector('.input-password');
+inp.addEventListener('input', () => console.log(inp.value))*/
