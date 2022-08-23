e1661a82942e6abfa6bdfd7c4b57e967debf925f
vikuli <vikukulichka.terekhova@gmail.com>
2022-08-23 22:13:41 +0300

    feat: make the authorization page adaptive

--- src/authorization.ts
+++ src/authorization.ts
@@ -2,3 +2,6 @@ import './style/normalize.css';
 import './style/style.css';
 import './script/passwordView';
 import './script/bookAnimation';
+
+/*const a = [0, 'qwer', '', true, false];
+console.log(a.filter((el) => el));*/
