0c5d0d8b05e8ca630c88fa16284a38d6da9ff7f8
IhorNovikow <gosha.noviko2012@gmail.com>
2022-08-26 09:55:37 +0300

    feat: add audio, add timmy

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import './style/header/book.css';
 import './style/main.css';
 import './style/footer.css';
 import './style/reset.css';
+import './style/boy.css';
+import './style/info.css';
 
 import './script/startGame';
 import './style/gameStr.css';
 import './script/changeBackground';
 import './script/firtVisit';
+import './script/backgroundMusic';
+import './script/musicMainGame';
--- src/script/startGame.ts
+++ src/script/startGame.ts
@@ -1,10 +1,31 @@
+import { info11, info12 } from './info1';
 const gameStr = <HTMLElement>document.querySelector('.start-wrapper');
 const buttonStarGame = <HTMLButtonElement>document.querySelector('.button-star-game');
 
 buttonStarGame.addEventListener('click', () => {
     gameStr.classList.remove('displayNone');
     const cross = <HTMLElement>gameStr.querySelector('.cross-wrapper');
+    const test = <HTMLElement>gameStr.querySelector('.text');
+    test.innerHTML = info11;
     cross.addEventListener('click', () => {
         gameStr.classList.add('displayNone');
     });
 });
+
+function forwardStrGame() {
+    const btnForward = <HTMLElement>gameStr.querySelector('.btn-forward');
+    const test = <HTMLElement>gameStr.querySelector('.text');
+    btnForward.addEventListener('click', () => {
+        test.innerHTML = info12;
+    });
+}
+forwardStrGame();
+
+function backStrGame() {
+    const btnForward = <HTMLElement>gameStr.querySelector('.btn-back');
+    const test = <HTMLElement>gameStr.querySelector('.text');
+    btnForward.addEventListener('click', () => {
+        test.innerHTML = info11;
+    });
+}
+backStrGame();
