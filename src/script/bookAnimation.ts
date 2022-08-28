b7f8c5c32c0a86910866f1c12a6fbb1fd04c86ad
Jafr0 <zhek42@yandex.ru>
2022-08-28 21:59:41 +0300

    update: almost done backend

--- src/script/bookAnimation.ts
+++ src/script/bookAnimation.ts
@@ -1,4 +1,4 @@
-function bookAnimation(): void {
+export function bookAnimation(): void {
     const registrationButton: NodeListOf<Element> = document.querySelectorAll('.registration-button');
     registrationButton.forEach((el: Element) => {
         const pageFront = <HTMLElement>document.querySelector('.front');
@@ -42,5 +42,3 @@ function bookAnimation(): void {
         });
     });
 }
-
-bookAnimation();
