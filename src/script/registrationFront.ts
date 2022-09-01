a9dab5376171befc2d16a7e8b434cdc3f1735907
Ihar Novikau <gosha.noviko2012@gmail.com>
2022-09-01 09:39:37 +0300

    fix: fix bag with id and adaptiv

--- src/script/registrationFront.ts
+++ src/script/registrationFront.ts
@@ -1,5 +1,30 @@
 import { createUser } from './joinFrontBack/registration';
 
+const registration = document.querySelectorAll('.button-signin');
+
+registration.forEach((el) => {
+    el.addEventListener('click', async () => {
+        const parentBlock = <HTMLElement>el.parentNode;
+        const userLoginReg = <HTMLInputElement>parentBlock.childNodes[3];
+        const userPassReg01 = <HTMLElement>parentBlock.childNodes[7];
+        const userPassReg = <HTMLInputElement>userPassReg01.childNodes[1];
+
+        console.log('userPassReg.value', userPassReg.value);
+        console.log('userLoginReg', userLoginReg.value);
+        const userLogin = userLoginReg.value;
+        const userPassword = userPassReg.value;
+        const data = {
+            username: userLogin,
+            password: userPassword,
+        };
+
+        if (el.classList.contains('registration-button')) {
+            createUser(data);
+        }
+    });
+});
+
+/*
 const registration = document.querySelector('.registration-button') as HTMLElement;
 const userLoginReg = document.querySelector('.input-login') as HTMLInputElement;
 const userPassReg = document.querySelector('.input-password') as HTMLInputElement;
@@ -17,3 +42,4 @@ registration.addEventListener('click', async (event) => {
         createUser(data);
     }
 });
+*/
