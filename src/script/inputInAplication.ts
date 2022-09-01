c6db3370f5cc1aa26a06189760d84a9c99cbc0fa
Ihar Novikau <gosha.noviko2012@gmail.com>
2022-09-01 08:30:57 +0300

    fix: fix mistaks after merge

--- src/script/inputInAplication.ts
+++ src/script/inputInAplication.ts
@@ -1,19 +1,22 @@
 import { loginUser } from './joinFrontBack/login';
 
-const userLog = document.querySelector('.but-log') as HTMLElement;
-const userNameLog = document.querySelector('.username') as HTMLInputElement;
-const userPassLog = document.querySelector('.userpass') as HTMLInputElement;
+const userLog = document.querySelectorAll('.but-log');
+userLog.forEach((el) => {
+    el.addEventListener('click', async (event) => {
+        const registrationForm = <HTMLElement>el.parentNode;
+        const userNameLog = <HTMLInputElement>registrationForm.querySelector('.entered-name');
+        const userPassLog = <HTMLInputElement>registrationForm.querySelector('.entered-password');
+        const target = event.target as Element;
+        const userPassword = userNameLog.value;
+        const userLogin = userPassLog.value;
+        const data = {
+            username: userPassword,
+            password: userLogin,
+        };
 
-userLog.addEventListener('click', async (event) => {
-    const target = event.target as Element;
-    const userPassword = userNameLog.value;
-    const userLogin = userPassLog.value;
-    const data = {
-        username: userPassword,
-        password: userLogin,
-    };
-
-    if (target.classList.contains('but-log')) {
-        loginUser(data);
-    }
+        if (target.classList.contains('but-log')) {
+            console.log('отправили запрос', data);
+            loginUser(data, registrationForm);
+        }
+    });
 });
