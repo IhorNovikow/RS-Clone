1e173233c487b8ab0ed35d20ca1eaf91a905d5a6
Ihar Novikau <gosha.noviko2012@gmail.com>
2022-09-02 18:04:11 +0300

    fix: fix some problem with import

--- src/gameJS.ts
+++ src/gameJS.ts
@@ -5,8 +5,5 @@ import './style/game/windowGame.css';
 import './style/game/windowQestion.css';
 import './style/game/windowAnswer.css';
 
-import './script/dopGame/info/allInfo';
-import './script/dopGame/answers/allanswers';
-
 import './script/dopGame/startDopGame';
 import './script/dopGame/openQestion';
